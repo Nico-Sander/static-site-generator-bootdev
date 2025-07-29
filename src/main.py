b8f2f4f630a152555a4916c6bd42ed0420011e88
Nico-Sander <sandernico512@gmail.com>
2025-07-29 12:18:19 +0200

    Completed CH4 L1

--- src/main.py
+++ src/main.py
@@ -1,10 +1,39 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    clean_copy_to_public() 
+
+def clean_copy_to_public():
+    # Get an empty public directory
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+
+    if os.listdir("public") != []:
+        raise Exception("public/ directory is not empty.")
+
+    # Copy files from static to public recursively
+    if os.path.exists("static") and os.listdir("static") != []:
+        copy_recursive("static", "public")
+
+def copy_recursive(src_path, dest_path):
+    src_subpaths = os.listdir(src_path)
+    src_subpaths, dest_subpaths = [os.path.join(src_path, i) for i in src_subpaths], [os.path.join(dest_path, i) for i in src_subpaths]
+
+    for i in range(len(src_subpaths)):
+        if os.path.isfile(src_subpaths[i]):
+            shutil.copy(src_subpaths[i], dest_subpaths[i])
+        else:
+            os.mkdir(dest_subpaths[i])
+            copy_recursive(src_subpaths[i], dest_subpaths[i])
+    
 
 
-main()
 
+
+
+
+if __name__ == "__main__":
+    main()
