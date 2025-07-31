1264ae73b040a3ab7bef1de8d3ec74e372b79100
Nico-Sander <sandernico512@gmail.com>
2025-07-31 12:05:44 +0200

    Completed CH5 L3, copied solution filed, might have to revert to own solution

--- src/main.py
+++ src/main.py
@@ -1,42 +1,30 @@
 import os
 import shutil
-from generate_page import generate_page
+
+from copy_static import copy_files_recursive
+from gencontent import generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
-    clean_copy_to_public()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 
-def clean_copy_to_public():
-    # Get an empty public directory
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
+main()
 
-    if os.listdir("public") != []:
-        raise Exception("public/ directory is not empty.")
-
-    # Copy files from static to public recursively
-    if os.path.exists("static") and os.listdir("static") != []:
-        copy_recursive("static", "public")
-
-def copy_recursive(src_path, dest_path):
-    src_subpaths = os.listdir(src_path)
-    src_subpaths, dest_subpaths = [os.path.join(src_path, i) for i in src_subpaths], [os.path.join(dest_path, i) for i in src_subpaths]
-
-    for i in range(len(src_subpaths)):
-        if os.path.isfile(src_subpaths[i]):
-            shutil.copy(src_subpaths[i], dest_subpaths[i])
-        else:
-            os.mkdir(dest_subpaths[i])
-            copy_recursive(src_subpaths[i], dest_subpaths[i])
-    
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
