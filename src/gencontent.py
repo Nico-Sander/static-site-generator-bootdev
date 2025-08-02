2fba0ee810415816615b781f398807283019d9ed
Nico-Sander <sandernico512@gmail.com>
2025-08-02 09:31:27 +0200

    Completed CH5 L4

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,5 @@
 import os
+from os.path import isdir
 from markdown_blocks import markdown_to_html_node
 
 
@@ -24,6 +25,21 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    files = os.listdir(dir_path_content)
+    for file in files:
+        print(f"Found file: {file}")
+        if os.path.isfile(os.path.join(dir_path_content, file)) and file[-3:] == ".md":
+            print(f"--- File is markdown file, TODO: generate html")
+            generate_page(os.path.join(dir_path_content, file), template_path, os.path.join(dest_dir_path, file[0:-3]+".html"))
+        elif os.path.isdir(os.path.join(dir_path_content, file)):
+            print(f"--- File is directory, TODO: stepping into the directory to look for .md files.")
+            generate_pages_recursive(os.path.join(dir_path_content, file), template_path, os.path.join(dest_dir_path, file))
+
+            
+
+
 
 
 def extract_title(md):
